eebad26ae5b386673eead3bd9278c078bc19445b
denghankun <denghankun@denghankun-virtual-machine.(none)>
2012-04-24 00:27:44 +0800

    lslsls

--- s4-managerV2/src/io/s4/manager/core/CommEventCallback.java
+++
@@ -1,9 +0,0 @@
-package io.s4.manager.core;
-
-import java.util.Map;
-
-public interface CommEventCallback {
-	
-	public void handleCallback(Map<String, Object> eventData);
-
-}
--- s4-managerV2/src/io/s4/manager/util/Constants.java
+++
@@ -1,8 +0,0 @@
-package io.s4.manager.util;
-
-public class Constants {
-	public static final String CONF_PATH = "./conf";
-	public static final String CLUSTER_FILE = "clusters.xml";
-	public static final String MACHINE_FILE = "machinelist.xml";
-	public static final String SUB_MACHINE_FILE = "submachinelist";
-}
